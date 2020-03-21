f47263553ff7857a2fa41f8e12af01dcbfc2d6aa
Dominik Horn <dominik.horn93@gmail.com>
2020-03-21 15:21:51 +0100

    Method Security

--- src/main/java/io/remedymatch/RmBeApplication.java
+++ src/main/java/io/remedymatch/RmBeApplication.java
@@ -2,6 +2,7 @@ package io.remedymatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true)

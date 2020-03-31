b5e8505559244370750531f76afa025db93199bd
Dominik Horn <dominik.horn93@gmail.com>
2020-04-01 00:36:02 +0200

    matchcontroller

--- src/main/java/io/remedymatch/match/api/MatchController.java
+++ src/main/java/io/remedymatch/match/api/MatchController.java
@@ -2,7 +2,6 @@ package io.remedymatch.match.api;
 
 import io.remedymatch.match.domain.MatchRepository;
 import io.remedymatch.match.domain.MatchService;
-import io.remedymatch.match.domain.MatchStatus;
 import io.remedymatch.person.domain.PersonRepository;
 import io.remedymatch.shared.GeoCalc;
 import io.remedymatch.web.UserProvider;
@@ -28,25 +27,6 @@ public class MatchController {
     private final MatchRepository matchRepository;
 
     @GetMapping
-    public ResponseEntity<List<MatchDTO>> alleMatchesLaden() {
-        val user = personRepository.findByUsername(userProvider.getUserName());
-
-        val matches = matchRepository.findAllByStatus(MatchStatus.Offen).stream()
-                .map(MatchMapper::mapToDTO)
-                .collect(Collectors.toList());
-
-        matches.forEach(m -> {
-            var entfernung = GeoCalc.kilometerBerechnen(MatchStandortMapper.mapToEntity(m.getStandortVon()), MatchStandortMapper.mapToEntity(m.getStandortAn()));
-            m.setEntfernung(entfernung);
-        });
-
-        return ResponseEntity.ok(matchService.beteiligteMatches(user.getInstitution())
-                .stream()
-                .map(MatchMapper::mapToDTO)
-                .collect(Collectors.toList()));
-    }
-
-    @GetMapping("/assigned")
     public ResponseEntity<List<MatchDTO>> beteiligteMatches() {
         val user = personRepository.findByUsername(userProvider.getUserName());
 

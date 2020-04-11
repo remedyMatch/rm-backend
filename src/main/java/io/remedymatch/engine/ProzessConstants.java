2b5fcc01d0af173e1873df79541e12139e267962
Matus Mala <matus.mala@gmail.com>
2020-04-11 20:48:55 +0200

    Nicht mehr benutzt

--- src/main/java/io/remedymatch/engine/ProzessConstants.java
+++
@@ -1,7 +0,0 @@
-package io.remedymatch.engine;
-
-public class ProzessConstants {
-
-    public final static String PROZESS_TYP_BEDARF = "BEDARF";
-    public final static String PROZESS_TYP_ANGEBOT = "ANGEBOT";
-}

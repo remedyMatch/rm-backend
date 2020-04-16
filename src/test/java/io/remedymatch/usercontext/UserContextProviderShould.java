3cddf6b936a9a7184cad73a3efee393708899aec
Matus Mala <matus.mala@gmail.com>
2020-04-16 16:47:08 +0200

    Falsche Import gelöst

--- src/test/java/io/remedymatch/usercontext/UserContextProviderShould.java
+++ src/test/java/io/remedymatch/usercontext/UserContextProviderShould.java
@@ -1,9 +1,7 @@
 package io.remedymatch.usercontext;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -33,6 +31,6 @@ public class UserContextProviderShould {
 	@WithMockJWT(usernameClaim = "myUsername")
 	public void username_aus_JWT_zurueckliefern() {
 
-		assertEquals(myUsername, userProvide.getUserName());
+		assertEquals("myUsername", userProvide.getUserName());
 	}
 }

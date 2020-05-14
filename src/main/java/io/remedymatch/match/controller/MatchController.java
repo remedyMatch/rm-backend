4f445594fe773e48a150873e37d796918a071214
Dominik Horn <dominik.horn93@gmail.com>
2020-05-14 14:22:03 +0200

    Match für Id laden

--- src/main/java/io/remedymatch/match/controller/MatchController.java
+++ src/main/java/io/remedymatch/match/controller/MatchController.java
@@ -1,14 +1,17 @@
 package io.remedymatch.match.controller;
 
+import io.remedymatch.domain.ObjectNotFoundException;
 import io.remedymatch.match.domain.MatchService;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -21,6 +24,15 @@ public class MatchController {
     public ResponseEntity<List<MatchRO>> beteiligteMatchesLaden() {
         val beteiligteMatches = matchService.ladeBeteiligteMatches();
         return ResponseEntity.ok(MatchMapper.map(beteiligteMatches));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatchRO> matchLaden(@PathVariable("id") UUID matchId) {
+        try {
+            return ResponseEntity.ok(MatchMapper.map(matchService.ladeMatch(matchId)));
+        } catch (ObjectNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
